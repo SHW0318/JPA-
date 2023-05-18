f8a25686f527193b41c26d4ba95874df13acd353
ici <ici@DESKTOP-J6RS3LU>
2023-05-18 15:50:00 +0900

    JPA BOARD

---
+++ src/main/java/com/company/board/JpaboardProjectHwApplication.java
@@ -0,0 +1,13 @@
+package com.company.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaboardProjectHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaboardProjectHwApplication.class, args);
+	}
+
+}
